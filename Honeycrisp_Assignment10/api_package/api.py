58ef45f6d297067f5005c1e2b55af9a4ccbbd1a8
Cam Shinker <shinkecj@mail.uc.edu>
2025-04-03 08:35:58 -0400

    Add project files.

---
+++ Honeycrisp_Assignment10/api_package/api.py
@@ -0,0 +1,13 @@
+# File Name: api.py
+# Student Name: Cam Shinker
+# email: shinkecj@mail.uc.edu
+# Assignment Number: Assignment 10
+# Due Date: 4/10/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Connect to an API and complete some tasks
+
+# Brief Description of what this module does: Contains functions/classes that complete the assignment tasks
+# Citations: 
+
+# Anything else that's relevant:
---
+++ Honeycrisp_Assignment10/main_package/main.py
@@ -0,0 +1,16 @@
+# File Name: main.py
+# Student Name: Cam Shinker
+# email: shinkecj@mail.uc.edu
+# Assignment Number: Assignment 10
+# Due Date: 4/10/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Connect to an API and complete some tasks
+
+# Brief Description of what this module does: Contains the entry point code for the assignment
+# Citations: 
+
+# Anything else that's relevant:
+
+if __name__ == "__main__":
+    pass
